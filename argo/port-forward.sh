69419c594aa202d2242440db3293c1c02fb0b792
Bernardo Lopes <bernardo@linuxadmin.cloud>
2023-01-18 09:10:52 -0300

    feat(argocd): add password-getter script in argocd startup

--- argo/port-forward.sh
+++ argo/port-forward.sh
@@ -6,7 +6,10 @@ then
   echo "Saindo."
   exit
 fi
-
+PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+echo "O usuário para log-in é admin"
+echo "A senha para o Argo é  $PASSWORD"
+echo "Use ela para se autenticar em localhost:8080"
 kubectl port-forward svc/argocd-server -n argocd 8080:80
 
 
